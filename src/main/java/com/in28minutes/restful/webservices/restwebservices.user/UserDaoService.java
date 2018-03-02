192495bd6ea597af008f44f93d37ffeded724430
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-03-02 12:36:01 +0300

    Create a restful web service using spring.

---
+++ src/main/java/com/in28minutes/restful/webservices/restwebservices.user/UserDaoService.java
@@ -0,0 +1,44 @@
+package com.in28minutes.restful.webservices.restwebservices.user;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+
+    private static List<User> users = new ArrayList<>();
+
+    private static int userCount = 4;
+
+    static {
+        users.add(new User(1, "Adam", new Date()));
+        users.add(new User(2, "Adam", new Date()));
+        users.add(new User(3, "Bill", new Date()));
+        users.add(new User(4, "Betty", new Date()));
+    }
+
+    public static List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        if(user.getId()==null){
+            user.setId(++userCount);
+        }
+        users.add(user);
+        return user;
+    }
+
+    public User findOne(int id){
+        for(User user:users){
+            if(user.getId()==id){
+                return user;
+            }
+        }
+        return null;
+    }
+
+}
