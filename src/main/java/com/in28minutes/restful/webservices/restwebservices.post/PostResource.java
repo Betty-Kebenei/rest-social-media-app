ffbe5fd710f94a678b95af30f68ea0102104c796
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-03-05 21:55:08 +0300

    add features to post and get posts of a specific user.

--- src/main/java/com/in28minutes/restful/webservices/restwebservices.post/PostResource.java
+++ src/main/java/com/in28minutes/restful/webservices/restwebservices.post/PostResource.java
@@ -1,4 +1,24 @@
 package com.in28minutes.restful.webservices.restwebservices.post;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class PostResource {
+
+    @Autowired
+    private PostDaoService service;
+
+    @GetMapping("/users/{userId}/posts")
+    public List<Post> retrieveAllPosts(@PathVariable Integer userId){
+        List<Post> posts = service.findAllPosts(userId);
+        return posts;
+
+    }
+
 }
